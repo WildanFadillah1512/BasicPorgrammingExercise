d1175cb21f5087e62cc1c5c9fc59b73472ef232f
Wildan Fadillah <160717220+WildanFadillah1512@users.noreply.github.com>
2024-03-28 21:41:01 +0700

    Session 7

---
+++ task7_2.py
@@ -0,0 +1,2 @@
+name = str(input("input your name : "))
+print("reversed name is", name[::-1])  
---
+++ task7_3.py
@@ -0,0 +1,3 @@
+list = [10, 20, 5, 40, 30, 72, 34]
+print("Largest Number is  : ",  max(list))
+print("Smallest Number is : ", min(list))
